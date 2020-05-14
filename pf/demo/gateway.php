034b648a994ff6753413f6f7a046e8ce3919e96e
hxr4567 <455647807@qq.com>
2020-05-14 13:55:44 +0800

    码商金额设置

--- pf/demo/gateway.php
+++ pf/demo/gateway.php
@@ -8,14 +8,14 @@ include ('sign.php');
 //请求获取的网页类型，json 返回json数据，text直接跳转html界面支付，如没有特殊需要，建议默认text即可
 $content_type = 'text';
 //商户ID->到平台首页自行复制粘贴
-$account_id = '10096';
+$account_id = '10101';
 //S_KEY->商户KEY，到平台首页自行复制粘贴，该参数无需上传，用来做签名验证和回调验证，请勿泄露
-$s_key = '9FEB729370EB98';
+$s_key = '8B7EB801AE0B07';
 //订单号码->这个是四方网站发起订单时带的订单信息，一般为用户名，交易号，等字段信息
 $out_trade_no = date("YmdHis") . mt_rand(10000,99999);
 //支付通道：支付宝（公开版）：alipay_auto、微信（公开版）：wechat_auto、服务版（免登陆/免APP）：service_auto
 $type = intval($_POST['type']);
-if($type == "1"){
+/*if($type == "1"){
 $thoroughfare = 'alipay_auto';
 }else if($type == "2"){
   $thoroughfare = 'alipayhongbao_auto';
@@ -50,14 +50,14 @@ $thoroughfare = 'alipay_auto';
 }else if($type == "17"){
   $thoroughfare = 'taobaodf_auto';
 }else if($type == "18"){
-  $thoroughfare = 'paofen_auto';
+  $thoroughfare = 'huafei_auto';
 }else if($type == "19"){
   $thoroughfare = 'wechatzs_auto';
 }else if($type == "20"){
   $thoroughfare = 'wechatphone_auto';
 
-}
-
+}*/
+$thoroughfare = 'paofen_auto';
 
 
 //支付金额
@@ -68,13 +68,13 @@ $sign = sign($s_key, ['amount'=>$amount,'out_trade_no'=>$out_trade_no]);
 $robin = 1;
 $use_city = 2;
 //微信设备KEY，新增加一条支付通道，会自动生成一个device Key，可在平台的公开版下看见，如果为轮训状态无需附带此参数，如果$robin参数为1的话，就必须附带设备KEY，进行单通道支付
-$device_key = 'C23C27ADBAF7F1EF55';
+$device_key = '5EB53550CE1ED2A9E5';
 //异步通知接口url->用作于接收成功支付后回调请求
-$callback_url = 'http://pf.zhiyungk.com/demo/notify.php';
+$callback_url = 'http://xin.com/demo/notify.php';
 //支付成功后自动跳转url
-$success_url = 'http://pf.zhiyungk.com/demo';
+$success_url = 'http://xin.com/demo';
 //支付失败或者超时后跳转url
-$error_url = 'http://pf.zhiyungk.com/demo';
+$error_url = 'http://xin.com/demo';
 //支付类型->类型参数是服务版使用，公开版无需传参也可以
 
 ?>
@@ -85,7 +85,7 @@ $error_url = 'http://pf.zhiyungk.com/demo';
 	<title>接口调用</title>
 </head>
 <body>
-<form action="http://pf.zhiyungk.com/gateway/index/checkpoint.do" method="post" id="frmSubmit">
+<form action="http://xin.com/gateway/index/checkpoint.do" method="post" id="frmSubmit">
     <input type="hidden" name="account_id" value="<?php echo $account_id;?>" />
 	<input type="hidden" name="content_type" value="<?php echo $content_type;?>"/>
 	<input type="hidden" name="thoroughfare" value="<?php echo $thoroughfare?>"/>
