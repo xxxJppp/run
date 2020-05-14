32363f3dd9c83bbe217e24b7e4408dce34bd1ceb
cm2859 <64074028+cm2859@users.noreply.github.com>
2020-05-14 13:38:11 +0800

    Default Changelist

--- pf/demo/gateway.php
+++ pf/demo/gateway.php
@@ -8,9 +8,9 @@ include ('sign.php');
 //请求获取的网页类型，json 返回json数据，text直接跳转html界面支付，如没有特殊需要，建议默认text即可
 $content_type = 'text';
 //商户ID->到平台首页自行复制粘贴
-$account_id = '10034';
+$account_id = '10096';
 //S_KEY->商户KEY，到平台首页自行复制粘贴，该参数无需上传，用来做签名验证和回调验证，请勿泄露
-$s_key = 'A4E823E7DE9D41';
+$s_key = '9FEB729370EB98';
 //订单号码->这个是四方网站发起订单时带的订单信息，一般为用户名，交易号，等字段信息
 $out_trade_no = date("YmdHis") . mt_rand(10000,99999);
 //支付通道：支付宝（公开版）：alipay_auto、微信（公开版）：wechat_auto、服务版（免登陆/免APP）：service_auto
@@ -50,7 +50,7 @@ $thoroughfare = 'alipay_auto';
 }else if($type == "17"){
   $thoroughfare = 'taobaodf_auto';
 }else if($type == "18"){
-  $thoroughfare = 'huafei_auto';
+  $thoroughfare = 'paofen_auto';
 }else if($type == "19"){
   $thoroughfare = 'wechatzs_auto';
 }else if($type == "20"){
@@ -65,16 +65,16 @@ $amount = floatval($_POST['amount']);
 //生成签名
 $sign = sign($s_key, ['amount'=>$amount,'out_trade_no'=>$out_trade_no]);
 //轮训状态，是否开启轮训，状态 1 为关闭   2为开启
-$robin = 2;
+$robin = 1;
 $use_city = 2;
 //微信设备KEY，新增加一条支付通道，会自动生成一个device Key，可在平台的公开版下看见，如果为轮训状态无需附带此参数，如果$robin参数为1的话，就必须附带设备KEY，进行单通道支付
-$device_key = '';
+$device_key = 'C23C27ADBAF7F1EF55';
 //异步通知接口url->用作于接收成功支付后回调请求
-$callback_url = 'http://xxa.erinqak.cn/demo/notify.php';
+$callback_url = 'http://pf.com/demo/notify.php';
 //支付成功后自动跳转url
-$success_url = 'http://xxa.erinqak.cn/demo';
+$success_url = 'http://pf.com/demo';
 //支付失败或者超时后跳转url
-$error_url = 'http://xxa.erinqak.cn/demo';
+$error_url = 'http://pf.com/demo';
 //支付类型->类型参数是服务版使用，公开版无需传参也可以
 
 ?>
@@ -85,7 +85,7 @@ $error_url = 'http://xxa.erinqak.cn/demo';
 	<title>接口调用</title>
 </head>
 <body>
-<form action="http://xxa.erinqak.cn/gateway/index/checkpoint.do" method="post" id="frmSubmit">
+<form action="http://pf.com/gateway/index/checkpoint.do" method="post" id="frmSubmit">
     <input type="hidden" name="account_id" value="<?php echo $account_id;?>" />
 	<input type="hidden" name="content_type" value="<?php echo $content_type;?>"/>
 	<input type="hidden" name="thoroughfare" value="<?php echo $thoroughfare?>"/>
