0f8f86a29c2a08e0340b63b967bfd009ddeedc48
s <s>
2020-05-18 16:34:16 +0800

    申诉订单状态增加处理中

--- pf/run/mashang/view/paofen/appeallist.php
+++ pf/run/mashang/view/paofen/appeallist.php
@@ -67,7 +67,7 @@ $fix = DB_PREFIX;
                     <span style="color: red">未审核</span>
                   <?php }else if($em['audit'] == 1){?>
                       <span style="color: red">已审核</span>
-                  <?php }else if($em['audit'] == 2){?>
+                  <?php }else if($em['audit'] == 3){?>
                       <span style="color: red">处理中</span>
                   <?php }else{?>
                       <span style="color: green">已驳回</span>
