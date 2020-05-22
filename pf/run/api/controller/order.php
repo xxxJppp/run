9652a0c1208b904cf3a9928afe59dd0f03cc7a69
root <root@SC-201910221120.localdomain>
2020-05-22 17:34:29 +0800

    提现‘

--- pf/run/api/controller/order.php
+++ pf/run/api/controller/order.php
@@ -4,19 +4,25 @@ namespace xh\run\api\controller;
 
 use xh\library\functions;
 use xh\library\jwt;
+use xh\library\mysql;
 use xh\library\request;
 
 class order
 {
-
-    private $token = '';
+    protected $token = '';
+    protected $user;
+    protected $mysql;
 
     public function __construct()
     {
         $token = request::filter('server.HTTP_TOKEN');
-        $checktoken = jwt::verifyToken($token);
+        $token = $token ? : request::filter('server.HTTP_AUTHORIZATION');
+        //$checktoken = jwt::verifyToken($token);
+        $checktoken = jwt::verifyToken(trim(str_replace('Bearer', '', $token)));
         if ($checktoken) {
             $this->token = jwt::getToken($checktoken['sub']);
+            $this->mysql = new mysql();
+            $this->user = $this->mysql->query("client_user", "username='{$checktoken['sub']}'")[0];
         } else {
             functions::json(-1, '签名验证失败');
         }
